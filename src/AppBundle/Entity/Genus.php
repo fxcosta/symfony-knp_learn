60ef0ab5375972494dc116a25d5cbe467422739c
Webdown404 <que.babault@gmail.com>
2016-07-09 23:14:35 +0200

    Database Config and Automatic Table Creation

---
+++ src/AppBundle/Entity/Genus.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webdown
+ * Date: 09/07/2016
+ * Time: 21:31
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="genus")
+ */
+
+class Genus
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    private $name;
+
+}
