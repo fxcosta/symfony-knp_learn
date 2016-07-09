faf157c748bd46a20cc22ae0fcca3aab4f133e74
Webdown404 <que.babault@gmail.com>
2016-07-09 15:28:01 +0200

    JSON Response

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -4,7 +4,9 @@ namespace AppBundle\Controller;
 
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 class GenusController extends Controller
@@ -16,14 +18,27 @@ class GenusController extends Controller
     public function showAction($genusName)
     {
 
+        return $this->render('genus/show.html.twig', ['name' => $genusName, 'notes' => $notes]);
+    }
+
+    /**
+     * @Route("/genus/{genusName}/notes")
+     * @Method("GET")
+     */
+    public function getNotesAction()
+    {
         $notes = [
-            'Octopus asked me a riddles, outsmarted me',
-            'I counted 8 legs...',
-            'Inked',
-            'I\'ve got a game on Wii U'
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Where you come from?', 'Date' => 'Dec. 20 2015'],
+            ['id' => 2, 'username' => 'AquaWeaver', 'avatarUri' => '/images/ryan.jpeg', 'note' => 'Hi, im Ryan', 'Date' => 'Dec. 21 2015'],
+            ['id' => 1, 'username' => 'AquaPelham', 'avatarUri' => '/images/leanna.jpeg', 'note' => 'Hi, anyone there?', 'Date' => 'Dec. 22 2015']
         ];
 
-        return $this->render('genus/show.html.twig', ['name' => $genusName, 'notes' => $notes]);
+        $data = [
+            'notes' => $notes
+        ];
+
+        return new JsonResponse($data);
+
     }
 
 }
