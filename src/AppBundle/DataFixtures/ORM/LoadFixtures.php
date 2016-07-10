53115aa50693582c6d852477668817459b527af3
Webdown404 <que.babault@gmail.com>
2016-07-10 11:59:05 +0200

    Adding fixtures

---
+++ src/AppBundle/DataFixtures/ORM/LoadFixtures.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: webdown
+ * Date: 10/07/2016
+ * Time: 11:44
+ */
+
+namespace AppBundle\DataFixtures\ORM;
+
+
+use AppBundle\Entity\Genus;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AppBundle\Entity\User;
+use Nelmio\Alice\Fixtures;
+use Nelmio\Alice\Fixtures\Fixture;
+
+class LoadFixtures implements FixtureInterface
+{
+    public function load(ObjectManager $manager)
+    {
+        Fixtures::load(__DIR__ . '/fixtures.yml', $manager);
+    }
+}
