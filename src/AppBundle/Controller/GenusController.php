bf419a52adf8cb51f74de45df31a5f7431fa2fd1
Webdown404 <que.babault@gmail.com>
2016-07-10 15:04:21 +0200

    Querying on a Relationship

--- src/AppBundle/Controller/GenusController.php
+++ src/AppBundle/Controller/GenusController.php
@@ -54,10 +54,7 @@ class GenusController extends Controller
             throw $this->createNotFoundException("No genus found!");
         }
 
-        $recentNotes = $genus->getNotes()
-            ->filter(function(GenusNote $note) {
-                return $note->getCreatedAt() > new \DateTime('-3 months');
-            });
+        $recentNotes = $em->getRepository('AppBundle:GenusNote')->findAllRecentNotesForGenus($genus);
 
         return $this->render('genus/show.html.twig', [
             'genus' => $genus,
