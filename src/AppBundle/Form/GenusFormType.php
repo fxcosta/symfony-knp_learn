24ed42c5f80d48ce5e1cba89da88c21ce12c9d96
Webdown404 <que.babault@gmail.com>
2016-07-11 17:31:08 +0200

    Custom Query in EntityType

--- src/AppBundle/Form/GenusFormType.php
+++ src/AppBundle/Form/GenusFormType.php
@@ -8,7 +8,11 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\SubFamily;
+use AppBundle\Repository\SubFamilyRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,10 +21,21 @@ class GenusFormType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('name')
-            ->add('subFamily')
+            ->add('subFamily', EntityType::class, [
+                'class' => SubFamily::class,
+                'placeholder' => 'Choose a Sub Family',
+                'query_builder' => function(SubFamilyRepository $repo) {
+                    return $repo->createAlphabeticalQueryBuilder();
+                }
+            ])
             ->add('speciesCount')
             ->add('funFact')
-            ->add('isPublished')
+            ->add('isPublished', ChoiceType::class, [
+                'choices' => [
+                    'Yes' => true,
+                    'No' => false
+                ]
+            ])
             ->add('firstDiscoveredAt');
     }
 
---
+++ src/AppBundle/Repository/SubFamilyRepository.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AppBundle\Repository;
+
+use AppBundle\Entity\Genus;
+use AppBundle\Entity\SubFamily;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityRepository;
+
+class SubFamilyRepository extends EntityRepository
+{
+
+    public function createAlphabeticalQueryBuilder()
+    {
+        return $this->createQueryBuilder('sub_family')
+            ->orderBy('sub_family.name', 'ASC');
+    }
+
+}
